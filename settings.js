aeef4a2ac7ec90da86bd72162e780acbfb1adfdd
cyber366 <ch419970@gmail.com>
2025-12-29 01:30:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: '‎CYBER366',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '𝗖𝗬𝗕𝗘𝗥𝟯𝟲𝟲', // Your name
+  ownerNumber: '919567919237', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
